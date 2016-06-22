15125ed7998fc294f7f73de2cdd424f8915e6328
bananasplitter <tiana_pehle@yahoo.com>
2016-06-21 22:33:05 -0700

    jpoij

---
+++ nameSplitParse/nameSplitParse.core/Repository/NameRepository.cs
@@ -0,0 +1,32 @@
+﻿using nameSplitParse.core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nameSplitParse.core.Repository
+{
+    public class NameRepository
+    {
+        private static ObservableCollection<Name> names = new ObservableCollection<Name>();
+
+        public static Name Create()
+        {
+           Name name = new Name();
+
+           names.Add(name);
+
+            return name;
+
+        }
+        public static void Update(Name name, string firstName, string lastName, string fullName)
+        {
+            name.FirstName = firstName;
+            name.LastName = lastName;
+            name.FullName = fullName;
+        }
+
+    }
+}
