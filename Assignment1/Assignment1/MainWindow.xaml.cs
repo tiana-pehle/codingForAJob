e350b86090b9945442fff6ac9447a10cabc43629
bananasplitter <tiana_pehle@yahoo.com>
2016-06-23 01:23:32 -0700

    DONE!

---
+++ Assignment1/Assignment1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTime_Click(object sender, RoutedEventArgs e)
+        {
+            var now = DateTime.Now;
+
+            string message = "The date is " + now.ToShortDateString() + " and the time is " + now.ToLongTimeString();
+
+            MessageBox.Show(message);
+        }
+    }
+}
